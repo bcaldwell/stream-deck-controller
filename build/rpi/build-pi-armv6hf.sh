385e0866fbc9116a07e1493b84253359df487c90
Benjamin Caldwell <8838361+bcaldwell@users.noreply.github.com>
2022-11-12 12:24:13 -0800

    Bump versions of c library

--- build/rpi/build-pi-armv6hf.sh
+++ build/rpi/build-pi-armv6hf.sh
@@ -10,7 +10,7 @@ set -eux
 HIDAPI_VER="0.8.0~rc1+git20140818.d17db57+dfsg-2"
 LIBUSB_VER="1.0.22-2"
 LIBUDEV_VER="241-7~deb10u8+rpi1"
-LIBC_VER="2.28-10+rpi1+deb10u1"
+LIBC_VER="2.31-13+rpi1+deb11u3"
 OPENSSL_VER="1.1.1o-1"
 DEPS=( \
   "http://mirrordirector.raspbian.org/raspbian/pool/main/h/hidapi/libhidapi-libusb0_${HIDAPI_VER}_armhf.deb" \
